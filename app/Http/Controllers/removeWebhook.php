7fe4d69c816b61857bd386a3f8ac789daccdd600
irfan prabaswara <irfanprabaswara@gmail.com>
2018-01-07 21:55:17 +0700

    perubahan set dan remove

--- app/Http/Controllers/removeWebhook.php
+++ app/Http/Controllers/removeWebhook.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Telegram\Bot\Laravel\Facades\Telegram;
 
 class removeWebhook extends Controller
 {
--- app/Http/Controllers/setWebhook.php
+++ app/Http/Controllers/setWebhook.php
@@ -3,12 +3,13 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Telegram\Bot\Laravel\Facades\Telegram;
 
 class setWebhook extends Controller
 {
   public function setWebHook()
   {
-    $response = Telegram::setWebhook(['url' => 'https://0181eb8d.ngrok.io/502539981:AAE7FDMraFwOV40U8NNR4MLpIkmnE1J7r84/webhook']);
+    $response = Telegram::setWebhook(['url' => 'https://57c6ea32.ngrok.io/502539981:AAE7FDMraFwOV40U8NNR4MLpIkmnE1J7r84/webhook']);
     return $response;
   }
 }
